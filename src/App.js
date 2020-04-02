66787393df059e2f2f8d228834be2c7e31005163
Mathi Gatti <mathigatti@gmail.com>
2020-04-02 18:45:24 -0300

    fix cards

--- src/App.js
+++ src/App.js
@@ -1,39 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import reviews from './reviews.json';
+import reviews from './data/reviews.json';
+import no_image from './data/no-image.jpg';
 import 'bootstrap/dist/css/bootstrap.css';
 
+function getImage(image){
+  if (image.includes("filestackcontent")){
+    return image
+  } else {
+    return no_image 
+  }
+}
+
 function App() {
+  var options = { year: 'numeric', month: 'long'};
+
   return (
-        <div class="container-fluid">
-          <div class="col col-sm-6" id="reviews">
+        <div class="card-columns" align="center">
+
 
             {reviews.map((review, index)=>{
               return (
 
-                <div class="card">
-                  <div id="carouselExampleControls2" class="carousel slide" data-ride="carousel">
-                    <div class="carousel-inner">
-                      <div class="carousel-item active">
-                        <img class="d-block w-100" src={review.writer.avatar_url} alt="First slide"></img>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="card-body">
-                    <h5 class="card-title">{review.writer.name}</h5>
-                    <p class="card-text">{review.content}</p>
-                  </div>
+              <div class="card" >
+                <img class="card-img-top" src={getImage(review.writer.avatar_url)} alt="Card image cap"></img>
+                <div class="card-body">
+                  <h5 class="card-title">{review.writer.name}</h5>
+                  <p class="card-text">{review.content}</p>
                 </div>
+                <div class="card-footer">
+                  <small class="text-muted">{new Date(review.created_at*1000).toLocaleDateString("en-US",options)}</small>
+                </div>
+              </div>
 
               )
             })}
-
-          </div>
         </div>
 
   );
 }
 
 export default App;
+
+
+
+
+
+
