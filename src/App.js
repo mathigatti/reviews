0d1c3a6574b72a6e54231f0279404a4aabc2ecf1
Mathi Gatti <mathigatti@gmail.com>
2020-04-02 17:37:42 -0300

    reviews on react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import reviews from './reviews.json';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div class="container-fluid">
+          <div class="col col-sm-6" id="reviews">
+
+            {reviews.map((review, index)=>{
+              return (
+
+                <div class="card">
+                  <div id="carouselExampleControls2" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img class="d-block w-100" src={review.writer.avatar_url} alt="First slide"></img>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="card-body">
+                    <h5 class="card-title">{review.writer.name}</h5>
+                    <p class="card-text">{review.content}</p>
+                  </div>
+                </div>
+
+              )
+            })}
+
+          </div>
+        </div>
+
   );
 }
 
